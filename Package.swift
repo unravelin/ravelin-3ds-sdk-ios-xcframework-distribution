d7abedca71e0e779521778be7df1fac90db75f12
Mark Brightman <mark.brightman@ravelin.com>
2021-08-12 13:10:00 +0100

    add framework

---
+++ Package.swift
@@ -0,0 +1,17 @@
+// swift-tools-version:5.3
+import PackageDescription
+let package = Package(
+    name: "Ravelin3DS",
+    platforms: [
+        .iOS(.v11)
+    ],
+    products: [
+        .library(
+            name: "Ravelin3DS", 
+            targets: ["Ravelin3DS"])
+    ],
+    targets: [
+        .binaryTarget(
+            name: "Ravelin3DS", 
+            path: "Ravelin3DS.xcframework")
+    ])
