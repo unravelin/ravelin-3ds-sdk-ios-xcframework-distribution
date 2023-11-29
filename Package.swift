104f4ac6d1e018a20f6076db96c5241635f614b1
Mark Brightman <mark.brightman@ravelin.com>
2023-11-29 18:32:35 +0000

    1.0.1

--- Package.swift
+++ Package.swift
@@ -13,6 +13,6 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Ravelin3DS", 
-            url: "https://ravelin.mycloudrepo.io/repositories/threeds2service-ios/release/1.0.0/Ravelin3DS.xcframework.zip",
-            checksum: "3957dfe64fbf0681b4bce2ab6e1107135125822cf89580bc4516bd29418b98af")
+            url: "https://ravelin.mycloudrepo.io/repositories/threeds2service-ios/release/1.0.1/Ravelin3DS.xcframework.zip",
+            checksum: "2cc9991032561174f2a51db544c741e9bc2b53b332994a8b7f36289281fb4501")
     ])
